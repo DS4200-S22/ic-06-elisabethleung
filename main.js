7222008a43de4ee137c17cbd296fc2a806f7a5e2
elisabethleung <98117631+elisabethleung@users.noreply.github.com>
2022-02-10 13:25:07 -0500

    up to step 6

--- main.js
+++ main.js
@@ -8,7 +8,13 @@ Modified: 12/06/21
 
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
-    // You will need to modiffy the body of this function as described in the assignment Readme
+    // You will need to modify the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    console.log("Button 1 was clicked");
+    let newNum = Math.random();
+    let buttonDiv = document.getElementById("random_num")
+    buttonDiv.innerHTML = newNum;
+
+
+
 }
